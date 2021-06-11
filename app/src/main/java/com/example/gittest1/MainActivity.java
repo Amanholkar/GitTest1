fcf1783f029eae07e56170d5a5265debf3e53c1a
GunFly6 <amanholkar00@gmail.com>
2021-06-11 22:19:46 +0530

    This is owr third commit with error

--- app/src/main/java/com/example/gittest1/MainActivity.java
+++ app/src/main/java/com/example/gittest1/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String Name = "This is my name Aman";
+        today is not good day for me
     }
 }
